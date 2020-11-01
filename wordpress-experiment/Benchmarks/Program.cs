e8d1bf03c076bf16ab8919535f67faa61b73afe6
Robert Husák <robert@husak.cloud>
2020-11-01 10:30:43 +0100

    Use BenchmarkDotNet to measure WP run

---
+++ wordpress-experiment/Benchmarks/Program.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using Pchp.Core;
+using System;
+using System.IO;
+
+namespace Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<WpBenchmark>();
+        }
+    }
+
+    [LongRunJob]
+    [MemoryDiagnoser]
+    public class WpBenchmark
+    {
+        private MemoryStream BinaryOutput = new MemoryStream();
+        private StreamWriter TextOutput = new StreamWriter(new MemoryStream());
+
+        private Context InitContext()
+        {
+            Context.AddScriptReference(typeof(WP).Assembly);
+
+            BinaryOutput.Position = 0;
+            TextOutput.BaseStream.Position = 0;
+
+            using var ctx = Context.CreateConsole("index.php");
+            ctx.RootPath = ctx.WorkingDirectory = Path.GetFullPath(@"..\..\..\..\Website");
+            ctx.OutputStream = BinaryOutput;
+            ctx.Output = TextOutput;
+            ctx.Server["HTTP_HOST"] = "localhost";
+            ctx.Server["REQUEST_URI"] = "/";
+
+            return ctx;
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Default()
+        {
+            var ctx = InitContext();
+            var index = Context.TryGetDeclaredScript("index.php");
+
+            try
+            {
+                index.Evaluate(ctx, PhpArray.NewEmpty(), null);
+            }
+            catch (ScriptDiedException died)
+            {
+                died.ProcessStatus(ctx);
+            }
+        }
+    }
+}
